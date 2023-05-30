1111b1d4076110851ddefe1e7c74f12baeb24cd9
Johnson <johnsonli1993@gmail.com>
2023-05-30 16:12:23 +0300

    support pandia

--- train.py
+++ train.py
@@ -1,4 +1,6 @@
 from rl_zoo3.train import train
+from pandia.agent.env import WebRTCEnv
+
 
 if __name__ == "__main__":
     train()
---
+++ train.sh
@@ -0,0 +1,1 @@
+PYTHONPATH=~/Workspace/Pandia python train.py --algo sac --env WebRTCEnv-v0 -tb tensorboard --save-freq 1000 --eval-freq 3000 --vec-env subproc --n-eval-envs 1
